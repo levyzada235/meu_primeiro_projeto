484f01682f9be756cb8ad7aa6906fda7a72b2daa
levyzada235 <levyaraujo235@gmail.com>
2025-11-27 17:39:22 -0300

    hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 # início do programa que pergunta o nome do usuário
 # e exibe uma mensagem de boas vindas
-name = str(input("Olá! Qual é o seu nome? ")
-  print(f"Muito prazer em te conhecer, {name}!")
+name = str(input("Olá! Qual é o seu nome? "))
+print(f"Muito prazer em te conhecer, {name}!")
            
 # idade do programa
 bot_age = 3
 # pergunta ao usuário quantos anos ele tem
 # e logo em seguida exibe a diferença de idade entre os dois, caso haja
-age = int(input(f"E quantos anos você tem {name}? ")
+age = int(input(f"E quantos anos você tem {name}? "))
 age_difference = (age - bot_age)
 if age == bot_age:
   print(f"Que coincidência! Eu também tenho {bot_age} anos!")
@@ -18,10 +18,9 @@ else:
 bot_color = ("Vermelho" and "vermelho")
 # pergunta a cor favorita do usuário
 # e exibe uma mensagem personalizada de acordo com a resposta do usuário
-color = str(input("Qual é a sua cor preferida? ")
+color = str(input("Qual é a sua cor preferida? "))
 if color == bot_color:
   print(f"Que bom gosto você tem! A minha cor favorita também é {bot_color}!")
 else:
-  print(f"Realmente {color} é uma cor muito bonita! A minha cor favorita é {bot_color}!"}
+  print(f"Realmente {color} é uma cor muito bonita! A minha cor favorita é {bot_color}!")
             
-
